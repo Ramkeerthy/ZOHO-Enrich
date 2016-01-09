0da31960340fa85d6748dcf42fe6b0687d9c21e4
Ramkeerthy <rkeerthya@gmail.com>
2016-01-09 13:39:52 +0530

    Update floyds.c

--- floyds.c
+++ floyds.c
@@ -1,1 +1,17 @@
 
+#include<stdio.h>
+int main()
+{
+  int i,j,k=1,n;
+  printf("\n Enter the number of lines to be printed:");
+  scanf("%d",&n);
+  for(i=1;i<=5;i++)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("%d\t",k++);
+    }
+    printf("\n");
+  }
+  return 0;
+}
